2f6f36a0f196fc65a49c10f870df414dac1590d6
Your Name <you@example.com>
2020-10-12 22:16:34 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = n.toString().split("").reverse();
+    if (arr[arr.length - 1] === "-") arr.pop();
+    return arr.join("");
 }
